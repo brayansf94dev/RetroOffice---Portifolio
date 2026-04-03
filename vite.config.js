558a6832a4293469783187b9086d7f40e6f2a2f4
brayansf94dev <brayanlucasdev@gmail.com>
2026-04-03 11:55:54 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // IMPORTANTE: se o repositório NÃO for usuario.github.io
-  // troque '/' pelo nome do seu repo, ex: '/portfolio-n64/'
-  base: '/',
+  base: '/RetroOffice---Portifolio/',
   build: {
     outDir: 'dist',
   },
